0fc3802a738e23ca366b4752e71da0330f1bba98
Farrux <Farrux@gmail.com>
2025-03-05 00:48:17 +0500

    implement product data fetching and UI rendering

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,9 @@
+import { getData } from "./fetchData.js";
+import { upDateUI } from "./upDataUI.js";
 const url = "https://dummyjson.com/product";
-const div = document.querySelector(".collection__container");
 
-function showData({ products }) {
-  products.forEach((product) => {
-    const { thumbnail, title, description, price } = product;
-    const card = document.createElement("div");
-    card.classList.add("card");
-    const image = document.createElement("img");
-    image.classList.add("card__image");
-    const cardTitle = document.createElement("h3");
-    cardTitle.classList.add("card__title");
-    const text = document.createElement("p");
-    text.classList.add("card__text");
-    //
-    image.src = thumbnail;
-    cardTitle.textContent = title;
-    text.textContent = description;
-    card.append(image, cardTitle, text);
-    div.appendChild(card);
-  });
-}
-fetch(url)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    showData(data);
-  })
+getData(url)
+  .then((data) => upDateUI(data))
   .catch((error) => {
     console.log(error);
   });
